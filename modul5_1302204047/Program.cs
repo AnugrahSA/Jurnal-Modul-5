7264288622ff736de2c2197c323b049599c8230b
anugrahSA <anugrahnugi321@gmail.com>
2022-03-26 15:12:09 +0800

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul5_1302204047/Program.cs
+++ modul5_1302204047/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace modul5_1302204047
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo("Spiderman");
+            SayaTubeVideo video2 = new SayaTubeVideo("Doctor Strange");
+            SayaTubeVideo video3 = new SayaTubeVideo("iron man");
+            SayaTubeVideo video4 = new SayaTubeVideo("Black Panther");
+            SayaTubeVideo video5 = new SayaTubeVideo("Captain Marvel");
+            SayaTubeVideo video6 = new SayaTubeVideo("Captain America");
+            SayaTubeVideo video7 = new SayaTubeVideo("World War z");
+            SayaTubeVideo video8 = new SayaTubeVideo("Doraemon");
+            SayaTubeVideo video9 = new SayaTubeVideo("Spiderman No Way Home");
+            SayaTubeVideo video10 = new SayaTubeVideo("Shang Chi");
+
+            SayaTubeUser user1 = new SayaTubeUser("Anugrah Saputra Aras");
+            user1.AddVideo(video1);
+            user1.AddVideo(video2);
+            user1.AddVideo(video3);
+            user1.AddVideo(video4);
+            user1.AddVideo(video5);
+            user1.AddVideo(video6);
+            user1.AddVideo(video7);
+            user1.AddVideo(video8);
+            user1.AddVideo(video9);
+            user1.AddVideo(video10);
+        }
+    }
+}
---
+++ modul5_1302204047/SayaTubeUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302204047
+{
+	public class SayaTubeUser
+	{
+		private int id;
+		private List<SayaTubeVideo> uploadedVideos;
+		public string Username;
+
+		public SayaTubeUser(string Username)
+		{
+			this.Username = Username;
+
+			
+
+
+		}
+
+		public int GetTotalVideoPlayCount()
+        {
+			return uploadedVideos.Count;
+        }
+
+		public void AddVideo(SayaTubeVideo video)
+        {
+			uploadedVideos.Add(video);
+        }
+
+		public void PrintAllVideoPlayCount()
+        {
+			Console.WriteLine("User: " + Username);
+			Console.WriteLine("Video 1 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 2 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 3 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 4 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 5 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 6 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 7 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 8 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 9 Judul: " + uploadedVideos.Count);
+			Console.WriteLine("Video 10 Judul: " + uploadedVideos.Count);
+		}
+	}
+}
